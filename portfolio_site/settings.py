74995598289a41d3f2fb5ea3a1a04ebda8485a0a
PalomaGit <pocapin995@g.educaand.es>
2025-10-22 15:36:43 +0200

    Mejorando blog

--- portfolio_site/settings.py
+++ portfolio_site/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-2=+rit&k=6$9&+=)6f&ri
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True').lower() == 'true'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1,172.31.17.93').split(',')
 
 
 # Application definition
